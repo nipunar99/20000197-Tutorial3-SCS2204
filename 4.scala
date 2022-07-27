1f3c74c36d774b557042692fd57e79b33bc03d31
Nipuna Rahal <89834520+nipunar99@users.noreply.github.com>
2022-07-27 16:54:04 +0530

    Update 4.scala

--- 4.scala
+++ 4.scala
@@ -1,5 +1,5 @@
 object main extends App{
-    def buyingCost(n:Int):Double = n*24.95*40/100;
+    def buyingCost(n:Int):Double = n*24.95*60/100;
     def shippingCost(n:Int):Double= if (n<=50) 3*n else 50*3+(n-50)*0.75;  
     def totalWholesaleCost(n:Int):Double = buyingCost(n)+shippingCost(n);
 
